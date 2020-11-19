d081091af3fcc98379bd0019c93b0b7998f6021d
Liniquer Silva <contato@liniker.com.br>
2020-11-19 07:50:41 -0300

    instalando ant design e node-sass

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Router from "./router";
+import 'antd/dist/antd.css';// ant design stylesheet
+import "./assets/scss/style.scss";
+import GlobalStyle from "./assets/globalStyle";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <Router />
   </React.StrictMode>,
   document.getElementById("root")
